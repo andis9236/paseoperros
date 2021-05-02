60418b41053ce8bdd8ade36ed90838a050dd21ec
Andis Andres Cardenas <62559653+andis9236@users.noreply.github.com>
2021-05-02 02:08:39 -0500

    SE AÑADE LA LISTA SE Y SE VERIFICA SU FUNCIONAMIENTO EN SWAGGER

--- src/main/java/com/umanizales/paseoperros/controller/CapacitacionController.java
+++ src/main/java/com/umanizales/paseoperros/controller/CapacitacionController.java
@@ -1,6 +1,7 @@
 package com.umanizales.paseoperros.controller;
 
 import com.umanizales.paseoperros.model.Empleado;
+import com.umanizales.paseoperros.service.ListaSEService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -9,29 +10,58 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 @RestController
 public class CapacitacionController
+
 {
-    @GetMapping
+    private ListaSEService listaSEService;
+
+    @Autowired
+    public CapacitacionController(ListaSEService listaSEService)
+    {
+        this.listaSEService = listaSEService;
+    }
+
+
+
+
+    @GetMapping(path="/count")
+    public int getCountEmployees()
+    {
+        return listaSEService.contarNodos();
+    }
+
+    @GetMapping(path="/listar")
+    public String getTotalEmployees()
+    {
+        return listaSEService.listarNodos();
+    }
+
+    /* @GetMapping
     public Empleado getEmployee()
     {
-        Empleado orozco= new Empleado("Andrés Orozco","1053478965",
-                3500000);
-        return orozco;
+        Empleado andis= new Empleado("Andis Andres Cardenas","56845613",
+                1500000);
+        return andis;
     }
     @GetMapping(path="/empleados")
     public Empleado[] getAllEmployees(){
         Empleado[] empleados= new Empleado[3];
-        empleados[0]= new Empleado("Andrés Orozco","1053478965",
+        empleados[0]= new Empleado("Andis Cardenas","56845613",
                 3500000);
-        empleados[1]= new Empleado("Carlos Loaiza","75147569",
+        empleados[1]= new Empleado("Luisa Mora","105845816",
                 1500000);
-        empleados[2]= new Empleado("Camila Castaño","24987547",
+        empleados[2]= new Empleado("Maria Gonzalez","854651658",
                 2500000);
         return empleados;
     }
+
     @PostMapping
     public Empleado crearEmpleado(@RequestBody Empleado employee)
     {
         employee.setSalario(employee.getSalario()*1.15);
         return employee;
     }
+
+     */
+
+
 }
---
+++ src/main/java/com/umanizales/paseoperros/model/excepcion/ListaSEExcepcion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.umanizales.paseoperros.model.excepcion;
+
+
+public class ListaSEExcepcion extends Exception{
+
+    public ListaSEExcepcion(String string) {
+        super(string);
+    }
+    
+}
